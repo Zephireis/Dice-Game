9c159cd0ca6690886a278e3fd6714fc89b94a93d
Travis C <Travisclarkakatc@hotmail.com>
2021-04-29 19:19:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1 //1-6
 
-var randomDiceImage = "images/dice" + randomNumber1 + ".png"
+var randomDiceImage = "./images/dice" + randomNumber1 + ".png"
 
 var image1 = document.querySelectorAll("img")[0]
 
@@ -11,7 +11,7 @@ image1.setAttribute("src", randomDiceImage);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1
 
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png"
+var randomDiceImage2 = "./images/dice" + randomNumber2 + ".png"
 
 var image2 = document.querySelectorAll("img")[1]
 
